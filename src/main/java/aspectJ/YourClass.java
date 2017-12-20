ff8d947c8d9ea907598646250fd1c91fb56f48e7
Samuel Chen <samuel.chen@backstopsolutions.com>
2017-12-20 01:54:22 -0600

    add declare warning and test Bridge method

--- src/main/java/aspectJ/YourClass.java
+++ src/main/java/aspectJ/YourClass.java
@@ -1,9 +1,14 @@
 package aspectJ;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class YourClass {
+public class YourClass extends Father<String>{
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("application-config.xml");
@@ -13,6 +18,7 @@ public class YourClass {
         yourClass.yourMethodAround(1);
         yourClass.yourMethodAround(1,"Test");
         yourClass.shouldNotBeUsed();
+        yourClass.printOutMethods();
     }
 
     public void yourMethodBefore() {
@@ -34,9 +40,30 @@ public class YourClass {
         System.out.println("x : "+x);
     }
 
+    public void printOutMethods(){
+        for (Method m :getClass().getDeclaredMethods()){
+            System.out.println(m.getReturnType()+" "+ m.getName()+" "+ m.isBridge());
+        }
+        System.out.println(Arrays.toString(getClass().getDeclaredMethods()));
+    }
+
     @Deprecated
     public void shouldNotBeUsed(){
         System.out.println("SHOULD NOT BE USED");
     }
 
+    @Override
+    String get() {
+        return null;
+    }
+
+    @Override
+    FileOutputStream getOutStream() {
+        try {
+            return new FileOutputStream("");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
